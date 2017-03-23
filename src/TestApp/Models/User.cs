d97f57846b44088cff06197fabd8776af2bf9785
Konstantin Olgin <kolgin@geeksforless.net>
2017-03-23 19:58:42 +0200

    created raw TestApp project

---
+++ src/TestApp/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace TestApp.Models
+{
+    public class User
+    {
+        public ObjectId Id { get; set; }
+
+        [BsonElement("UserId")]
+        public int UserId { get; set; }
+
+        [BsonElement("FirstName")]
+        public string FirstName { get; set; }
+
+        [BsonElement("LastName")]
+        public string LastName { get; set; }
+
+        [BsonElement("Age")]
+        public int Age { get; set; }
+
+        [BsonElement("Tags")]
+        public string[] Tags { get; set; }
+    }
+}
